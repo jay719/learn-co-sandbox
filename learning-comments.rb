584a2fc13b3cb4910d5e6fa815fa20952df7d16f
Javaria Brascom <javariab17@gmail.com>
2020-10-28 19:55:21 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,10 @@
+puts "I love mikayla"
+puts " so so so so "
+# puts "sike"
+puts "so so mmuch "
+
+super_ugly = {
+:hi=> Time.now
+:poop=> "poopydoop"
+}
+super_ugly
---
+++ while.rb
@@ -0,0 +1,18 @@
+3.times do
+  puts "I ran."
+end
+
+count = 0
+n = 3
+while true do
+  break if count >= n
+  puts "I ran."
+  count += 1
+end
+
+
+counter = 0
+until counter == 20
+  puts "The current number is less than 20."
+  counter += 1
+end
